667b24917be65843eff0849cbb5ec07472aa059e
Kanhaiya Patial <kanhaiyapatial@gmail.com>
2021-10-05 15:33:26 +0530

    Changes to filter by Address

---
+++ hello-world/src/main/java/com/mohsinkd786/dtos/AddressDto.java
@@ -0,0 +1,12 @@
+package com.mohsinkd786.dtos;
+import lombok.Data;
+@Data
+public class AddressDto {
+
+    private String city;
+
+    private String street;
+
+    private int zipCode;
+
+}
---
+++ hello-world/src/main/java/com/mohsinkd786/dtos/UserDto.java
@@ -0,0 +1,12 @@
+package com.mohsinkd786.dtos;
+
+import lombok.Data;
+
+@Data
+public class UserDto {
+    private int id;
+    private String name;
+    private int phoneNumber;
+    private AddressDto address;
+
+}
