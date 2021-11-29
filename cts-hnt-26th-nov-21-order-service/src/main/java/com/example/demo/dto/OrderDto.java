b1c14fa0a41f561b67a93cfcb61ec0c0a2f55f8e
Kanhaiya <kanhaiyapatial@gmail.com>
2021-11-29 11:21:07 +0530

    getter sette added

--- cts-hnt-26th-nov-21-order-service/src/main/java/com/example/demo/dto/OrderDto.java
+++ cts-hnt-26th-nov-21-order-service/src/main/java/com/example/demo/dto/OrderDto.java
@@ -1,16 +1,49 @@
 package com.example.demo.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
 public class OrderDto {
 	private String id;
 	private String orderName;
 	private Double orderPrice;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getOrderName() {
+		return orderName;
+	}
+
+	public void setOrderName(String orderName) {
+		this.orderName = orderName;
+	}
+
+	public Double getOrderPrice() {
+		return orderPrice;
+	}
+
+	public void setOrderPrice(Double orderPrice) {
+		this.orderPrice = orderPrice;
+	}
+
+	/**
+	 * @param id
+	 * @param orderName
+	 * @param orderPrice
+	 */
+	public OrderDto(String id, String orderName, Double orderPrice) {
+		super();
+		this.id = id;
+		this.orderName = orderName;
+		this.orderPrice = orderPrice;
+	}
+
+	public OrderDto() {
+		super();
+
+	}
+
 }
